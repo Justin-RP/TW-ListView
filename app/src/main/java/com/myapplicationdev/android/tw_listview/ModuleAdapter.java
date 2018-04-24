609abb47f18ab614a84f46a9720b7d3258e26104
16022916 <16022916@myrp.edu.sg>
2018-04-24 12:55:13 +0800

    Activity 2

--- app/src/main/java/com/myapplicationdev/android/tw_listview/ModuleAdapter.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/ModuleAdapter.java
@@ -1,12 +1,55 @@
 package com.myapplicationdev.android.tw_listview;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class ModuleAdapter {
+import java.util.ArrayList;
 
+public class ModuleAdapter extends ArrayAdapter<Modules> {
+
+    private ArrayList<Modules> modules;
     private Context context;
+    private TextView tvModule;
+    private ImageView ivImg;
 
+    public ModuleAdapter(Context context, int resource, ArrayList<Modules> object) {
+        super(context, resource, object);
+        // Store the food that is passed to this adapter
+        modules = object;
+        // Store Context object as we would need to use it later
+        this.context = context;
+    }
 
+    // getView() is the method ListView will call to get the View object every time ListView needs a row
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // The usual way to get the LayoutInflater object to "inflate" the XML file into a View object
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        // "Inflate" the row.xml as the layout for the View object
+        View rowView = inflater.inflate(R.layout.row, parent, false);
 
+        // Get the TextView object
+        tvModule = (TextView) rowView.findViewById(R.id.textViewName);
+        // Get the ImageView object
+        ivImg = (ImageView) rowView.findViewById(R.id.imageViewMod);
+        // The parameter "position" is the index of the row ListView is requesting. We get back the food at the same index.
+        Modules currentMod = modules.get(position);
+        // Set the TextView to show the food
+        tvModule.setText(currentMod.getName());
+        // Set the image to star or nostar accordingly
+        if(currentMod.isImg()) {
+            ivImg.setImageResource(R.drawable.prog);
+        }
+        else {
+            ivImg.setImageResource(R.drawable.nonprog);
+        }
+        // Return the nicely done up View to the ListView
+        return rowView;
 
+    }
 }
--- app/src/main/java/com/myapplicationdev/android/tw_listview/SecondActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/SecondActivity.java
@@ -3,27 +3,46 @@ package com.myapplicationdev.android.tw_listview;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class SecondActivity extends AppCompatActivity {
 
     ListView lv;
     TextView tvYear;
-
+    ArrayAdapter aa;
+    ArrayList<Modules> modulesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        lv = (ListView) this.findViewById(R.id.lvModules);
+        lv = (ListView) this.findViewById(R.id.lvModule);
         tvYear = (TextView) findViewById(R.id.tvYear);
 
         Intent i = getIntent();
         String year = i.getStringExtra("year");
         tvYear.setText(year);
 
+        if(year == "Year 1"){
+            modulesList.add(new Modules("C208",true));
+            modulesList.add(new Modules("C200",false));
+            modulesList.add(new Modules("C346",true));
+        } else if (year == "Year 2"){
+            modulesList.add(new Modules("C100",true));
+            modulesList.add(new Modules("C900",false));
+            modulesList.add(new Modules("C200",true));
+        } else {
+            modulesList.add(new Modules("C300",false));
+            modulesList.add(new Modules("C700",false));
+            modulesList.add(new Modules("C902",true));
+        }
+        aa = new ModuleAdapter(this, R.layout.row, modulesList);
+        lv.setAdapter(aa);
 
 
     }
